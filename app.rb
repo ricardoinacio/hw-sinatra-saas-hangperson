6a183bf5a75c918745937b696e02c4e84ccaeba5
ricardoinacio <ricardoinacio@me.com>
2015-11-09 00:00:02 +0000

    Acho que terminamos...

--- app.rb
+++ app.rb
@@ -40,6 +40,10 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if @game.guess(letter) == false
+      flash[:message] = "You have already used that letter."
+    end
+    
     redirect '/show'
   end
   
@@ -50,7 +54,15 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    state = @game.check_win_or_lose
+    case state
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
