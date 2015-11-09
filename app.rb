dd667e6542f1bcb8d87d9a8310ee44d8bb3c8610
ricardoinacio <ricardoinacio@me.com>
2015-11-09 04:41:22 +0000

    Edge cases + little refactoring

--- app.rb
+++ app.rb
@@ -40,8 +40,11 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if @game.guess(letter) == false
-      flash[:message] = "You have already used that letter."
+    begin
+      valid = @game.guess(letter)
+      flash[:message] = "You have already used that letter." unless valid
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
     end
     
     redirect '/show'
@@ -54,8 +57,7 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    state = @game.check_win_or_lose
-    case state
+    case @game.check_win_or_lose
     when :win
       redirect '/win'
     when :lose
@@ -67,11 +69,13 @@ class HangpersonApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :win
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :lose
     erb :lose # You may change/remove this line
   end
   
